137eff8d6f07caa9f5a53e27da26882eab91a2f0
Abhinay1121 <puppalaabhinay49@gmail.com>
2022-10-19 18:27:34 -0500

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+// components
+import Logo from "./Logo";
+import LocationSearch from "./LocationSearch";
+import ProfileSettings from "./ProfileSettings";
+import MobileSearch from "./MobileSearch";
+import {
+  flexBetweenCenter,
+  dFlex,
+  displayOnDesktop,
+} from "themes/commonStyles";
+
+const Header = () => {
+  return (
+    <Box
+      sx={{
+        ...dFlex,
+       
+        borderBottom: "1px solid #ddd",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Box
+          sx={{
+            ...flexBetweenCenter,
+            minHeight: 90,
+            px: 4,
+          }}
+        >
+          <Box sx={displayOnDesktop}>
+            <Logo />
+          </Box>
+          <Box sx={displayOnDesktop}>
+            <LocationSearch />
+          </Box>
+          <Box sx={displayOnDesktop}>
+            <ProfileSettings />
+          </Box>
+
+          <Box sx={{ display: { xs: "block", md: "none" } }}>
+            <MobileSearch />
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default Header;
